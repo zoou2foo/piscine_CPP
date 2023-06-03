7231af6afcd4ffce1fa879df3ccb14f43e25d7ae
Valerie <zoou2foo@gmail.com>
2023-06-03 12:31:51 -0400

    contact hpp almost done

--- module_00/ex01/Contact.hpp
+++ module_00/ex01/Contact.hpp
@@ -1,10 +1,33 @@
 #ifndef CONTACT_CLASS_H
 # define CONTACT_CLASS_H
 
-#include "PhoneBook.hpp"
+#include <string>
+#include <iostream>
+#include <iomanip>
 
+//faire ses getMachin et setMachin
 class Contact {
 
+	//functions will be called from outside
+	public:
+		void setFirstName(std::string _firstName);
+		void setLastName(std::string _lastName);
+		void setNickname(std::string _nickname);
+		void setPhoneNumber(std::string _phoneNumber);
+		void setDarkestSecret(std::string _darkestSecret);
+		std::string getFirstName(void) const; //in the function, I will give them the right var
+		std::string getLastName(void) const;
+		std::string getNickname(void) const;
+		std::string getPhoneNumber(void) const;
+		std::string getDarkestSecret(void) const;
+
+	//no need for an outsider to have access to those
+	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
 
 };
 
--- module_00/ex01/PhoneBook.hpp
+++ module_00/ex01/PhoneBook.hpp
@@ -1,14 +1,12 @@
 #ifndef PHONEBOOK_CLASS_H
 # define PHONEBOOK_CLASS_H
 
-#include <iostream>
 #include "Contact.hpp"
 
 class PhoneBook {
 	//functions add, search, exit
 	//iomanip and use setw to fix each column to 10 characters (width)
-	PhoneBook
-	
+
 	Contact tab[8][4];
 
 
