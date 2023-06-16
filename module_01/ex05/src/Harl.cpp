283318635f06d654abb1aae60579b949f47872bd
Valerie <zoou2foo@gmail.com>
2023-06-16 11:11:10 -0400

    cpp01 ex05 done

--- module_01/ex05/src/Harl.cpp
+++ module_01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 11:09:16 by vjean             #+#    #+#             */
-/*   Updated: 2023/06/15 15:09:09 by vjean            ###   ########.fr       */
+/*   Updated: 2023/06/16 11:06:02 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,26 @@ void	Harl::complain(std::string level)
 {
 	//select the correct call depending of level
 	//pointers to member functions
+	void (Harl::*PtrMemFn[4])() = { //create a pointer for an array of functions
+		&Harl::debug, //where I pass the address to call the function
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	std::string str_level[4] = { //array of strings to compare what I received in "level" and if it's in my arrray
+		"debug",
+		"info",
+		"warning",
+		"error"
+	};
+	for (int i = 0; i < 4; i++)
+	{
+		if (str_level[i] == level) //looking if what I got in level is in my str_level tab; then send the pointer to the correct function in the array
+		{
+			(this->*PtrMemFn[i])();
+			return;
+		}
+	}
 }
 
 void	Harl::debug(void)
--- module_01/ex05/src/main.cpp
+++ module_01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 11:09:28 by vjean             #+#    #+#             */
-/*   Updated: 2023/06/15 15:07:42 by vjean            ###   ########.fr       */
+/*   Updated: 2023/06/16 11:09:47 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,21 @@ int main(void)
 {
 	Harl Keven;
 
-	Keven.complain("debug");
-	Keven.complain("info");
-	Keven.complain("warning");
-	Keven.complain("error");
+	//first test
+	// std::cout << "Test 1 with debug" << std::endl;
+	// Keven.complain("debug");
+	// std::cout << "Test 2 with info" << std::endl;
+	// Keven.complain("info");
+	// std::cout << "Test 3 with warning" << std::endl;
+	// Keven.complain("warning");
+	// std::cout << "Test 4 with error" << std::endl;
+	// Keven.complain("error");
+
+	//if user choose
+	std::string levelChosen;
+	std::cout << "Please make a select between: debug, info, warning or error. Type your choice: " << std::endl;
+	std::cin >> levelChosen;
+	Keven.complain(levelChosen);
+
 	return 0;
 }
