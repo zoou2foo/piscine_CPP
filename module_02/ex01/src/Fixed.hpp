75e5614b528fcce2157001faf5cea5643952ac78
Valerie <zoou2foo@gmail.com>
2023-06-17 14:50:00 -0400

    worked a bit on ex01 cpp02

--- module_02/ex01/src/Fixed.hpp
+++ module_02/ex01/src/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 12:45:27 by vjean             #+#    #+#             */
-/*   Updated: 2023/06/17 14:34:32 by vjean            ###   ########.fr       */
+/*   Updated: 2023/06/17 14:49:27 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,22 @@ class Fixed {
 
 	public:
 		Fixed(void);
+		Fixed(int const ); //QUESTION: constructor taking an int const and convert it in fixed comma...
+		Fixed(float const num); //QUESTION: constructor with const float and convert it in fixed comma...
 		Fixed(Fixed const &src);
 		~Fixed(void);
 
 		Fixed &operator=(Fixed const &rhs);
 		int	getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
 
 	private:
 		int _fixedComma;
 		static const int _bitsNumber;
 };
 
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs); //need to verify this...
 
 #endif
---
+++ module_02/ex01/src/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 12:46:18 by vjean             #+#    #+#             */
+/*   Updated: 2023/06/17 14:42:15 by vjean            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main()
+{
+	Fixed		a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+}
+
+/* What it should print
+Default constructor called
+Int constructor called
+Float constructor called
+Copy constructor called
+Copy assignment operator called
+Float constructor called
+Copy assignment operator called
+Destructor called
+a is 1234.43
+b is 10
+c is 42.4219
+d is 10
+a is 1234 as integer
+b is 10 as integer
+c is 42 as integer
+d is 10 as integer
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+*/
