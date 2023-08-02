32a53f4aae6370b5bda4c4adeb2854d331784e46
Valerie <zoou2foo@gmail.com>
2023-08-02 14:22:25 -0400

    setup ex01 cpp07

---
+++ module_07/ex01/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 14:03:06 by vjean             #+#    #+#             */
+/*   Updated: 2023/08/02 14:20:54 by vjean            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Iter.hpp"
+
+//template function iter qui itère; print ce qu'il y a dans un tableau
+//peut importe le type
+//avoir un tableau de int, tableau de string...
+//autres idées de test?
+
+int main(void)
+{
+	std::cout << "-----Test with string array-----" << std::endl;
+	std::string stringArray[4] = { "hello", "mother", "fuckers", "!"};
+
+
+	std::cout << "-----Test with string array-----" << std::endl;
+	int stringArray[5] = { 124, 63, -2, 42, 0};
+}
