259b94fe11e8573953a4752b7ab3d61190696df4
Valerie <zoou2foo@gmail.com>
2023-08-02 16:00:37 -0400

    starting to create template ft ex01 cpp07

--- module_07/ex01/src/Iter.hpp
+++ module_07/ex01/src/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:03:01 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/02 14:03:48 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/02 15:58:42 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 
 #include <iostream>
 #include <iomanip>
+
+//first function: iter
+//iter takes 1. address of array; 2. length of array; 3. FUNCTION to look every element of the array
+//3rd param: can be a template function too.
+
+template<typename T>
+void	getArray(T index)
+{
+	
+}
+
+template<typename T, typename Func>
+void	iter(T const & array, T const length, Func getArray(T index))
+{
+	for()
+}
 
 
 #endif
