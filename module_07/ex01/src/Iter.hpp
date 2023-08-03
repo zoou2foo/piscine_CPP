57f332315cedc1e983b4f35cab7715c344f2aea0
Valerie <zoou2foo@gmail.com>
2023-08-03 09:17:03 -0400

    halfway done ex01cpp07

--- module_07/ex01/src/Iter.hpp
+++ module_07/ex01/src/Iter.hpp
@@ -6,31 +6,56 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:03:01 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/02 15:58:42 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/03 09:05:51 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
-#define ITER_HPP
+# define ITER_HPP
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 //first function: iter
 //iter takes 1. address of array; 2. length of array; 3. FUNCTION to look every element of the array
 //3rd param: can be a template function too.
 
 template<typename T>
-void	getArray(T index)
+void	getArray(T & array)
 {
-	
+	std::cout << "\033[32m" << array << std::endl;
+	std::cout << "\033[0m";
+	std::cout << "After modify what's in this spot: ";
+	array = 'a';
+	std::cout << "\033[31m" << array << std::endl;
+	std::cout << "\033[0m";
 }
 
-template<typename T, typename Func>
-void	iter(T const & array, T const length, Func getArray(T index))
-{
-	for()
-}
+// template<typename T>
+// void	printArray(const T & array)
+// {
+// 	std::cout << array << std::endl;
+// 	array = 'a';
+// 	std::cout << array << std::endl;
+// }
 
+// template<typename T, int>
+// void	iter(const T* array, int length, void(*printArray)(const T & array))
+// {
+// 	for(int index = 0; index < length; index++)
+// 		printArray(array[index]);
+// }
+
+
+template<typename T>
+void	iter(T * array, int length, void(*getArray)(T & array))
+{
+	for(int index = 0; index < length; index++)
+	{
+		std::cout << "index: " << index << std::endl;
+		getArray(array[index]);
+	}
+}
 
 #endif
--- module_07/ex01/src/main.cpp
+++ module_07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:03:06 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/02 14:20:54 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/03 09:16:36 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,49 @@
 
 int main(void)
 {
-	std::cout << "-----Test with string array-----" << std::endl;
+	std::cout << "\033[35m" << "------------------------------Test 1------------------------------" << std::endl;
+	std::cout << "\033[0m";
+	std::cout << "-------------Test with string array (non const)-------------" << std::endl;
+	std::cout << "\033[0m";
 	std::string stringArray[4] = { "hello", "mother", "fuckers", "!"};
+	std::cout << "First call=> ";
+	iter(stringArray, 2, getArray);
+	std::cout << "Second call=> ";
+	iter(stringArray, 3, getArray);
+	std::cout << "Third call=> ";
+	iter(stringArray, 4, getArray);
+
+	std::cout << "\033[33m" << "-----Test with string array (non const  & after changes)----" << std::endl;
+	std::cout << "\033[0m";
+	iter(stringArray, 4, getArray);
 
 
-	std::cout << "-----Test with string array-----" << std::endl;
-	int stringArray[5] = { 124, 63, -2, 42, 0};
+
+	std::cout << "\033[33m"  << "-------------Test with int array (const)-------------" << std::endl;
+	std::cout << "\033[0m";
+	int intArray[5] = { 124, 63, -2, 42, 0};
+	std::cout << "First call=> ";
+	iter(intArray, 1, getArray);
+	std::cout << "Second call=> ";
+	iter(intArray, 2, getArray);
+	std::cout << "Third call=> ";
+	iter(intArray, 3, getArray);
+	std::cout << "Fourth call=> ";
+	iter(intArray, 4, getArray);
+
+	std::cout << "\033[33m" << "-----Test with int array (non const  & after changes)----" << std::endl;
+	std::cout << "\033[0m";
+	iter(intArray, 5, getArray);
+
+	std::cout << "\033[35m" << "------------------------------Test 2------------------------------" << std::endl;
+	std::cout << "\033[0m";
+	// std::cout << "\033[33m" << "-----Test with string array (const)-----" << std::endl;
+	//std::cout << "\033[0m";
+	// const std::string stringArray[4] = { "hello", "mother", "fuckers", "!"};
+	// iter(stringArray, 1, printArray);
+	// iter(stringArray, 2, printArray);
+	// iter(stringArray, 3, printArray);
+	// iter(stringArray, 4, printArray);
+
+
 }
