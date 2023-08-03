43873c716bb16b12096070df8a0246386c757167
Valerie <zoou2foo@gmail.com>
2023-08-03 13:38:33 -0400

    setup for ex02 cpp07

--- module_07/ex02/src/Array.hpp
+++ module_07/ex02/src/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 11:08:28 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/03 11:09:20 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/03 13:37:45 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+template<typename T, unsigned int>
+class Array
+{
+	public:
+		Array(void); //Default: but need to create an empty array
+		Array(unsigned int n); //Constructor to create an array of n elements
+		Array(Array const & src);
+		~Array(void);
 
+		Array&			operator=(Array const & rhs);
+		unsigned int	size(void) const;
 
+	private:
+};
 
 #endif
