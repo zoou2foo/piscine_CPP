bb18bcd73032516731c8601b6fa7ee57d36ec450
Valerie <zoou2foo@gmail.com>
2023-08-10 15:43:49 -0400

    almost done ex02 cpp08 need more test

--- module_08/ex02/src/MutantStack.hpp
+++ module_08/ex02/src/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 11:11:59 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/10 12:47:59 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/10 15:37:01 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 #include <deque>
 
 template<typename T> //need to be a template to reproduce what stack does as we will just add iterator from deque
-class MutantStack : public std::deque<T> std::stack<T>
+class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack(void) : std::stack<T>() {}
-		MutantStack(MutanStack const & src) : std::stack<T>() { *this = src; }
+		MutantStack(MutantStack const & src) : std::stack<T>() { *this = src; }
 		~MutantStack(void) {}
 
 		MutantStack& operator=(MutantStack const & rhs){
@@ -31,8 +31,43 @@ class MutantStack : public std::deque<T> std::stack<T>
 			//might need to copy some stuff here
 			return (*this);
 		}
-		//std::stack<T>			_box;
+		//prototyper begin() and end()
+		// MutantStack<T>::iterator*	begin(void) {
+		// 	return (this->c.begin()); //only in adapter containers that have a c which represents a sub-container(sequential container)
+		// }
+		// MutantStack<T>::iterator*	end(void) {
+		// 	return (this->c.end());
+		// }
+		class iterator
+		{
+			public:
+				iterator(typename std::deque<T>::iterator it) : _myIterator(it) {}
+				iterator(iterator const & src) : _myIterator(src._myIterator) {}
+				~iterator(void) {}
 
+				iterator&	operator++(void) { //incremente
+					++this->_myIterator;
+					return (*this);
+				}
+				iterator&	operator--(void) { //decremente. I do not need it but the main is testing if my iterator will work properly
+					--this->_myIterator;
+					return(*this);
+				}
+				T&			operator*(void) {
+					return (*this->_myIterator);
+				}
+				bool		operator!=(iterator& rhs) {
+					return (this->_myIterator != rhs._myIterator);
+				}
+			private:
+				typename std::deque<T>::iterator	_myIterator;
+		};
+		iterator	begin(void) {
+			return (this->c.begin());
+		}
+		iterator	end(void) {
+			return (this->c.end());
+		}
 
 	private:
 
--- module_08/ex02/src/main.cpp
+++ module_08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 11:11:43 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/10 11:33:28 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/10 15:41:33 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int main()
 	mstack.push(5); //we add 5 in the stack
 	mstack.push(17); //we add 17 in the stack
 
-	std::cout << mstack.top() << std::endl; //we print what's on top
+	std::cout << "What's on top of the stack (after pushing 2 numbers): " << mstack.top() << std::endl; //we print what's on top
 
 	mstack.pop(); //we remove element from the top
 
-	std::cout << mstack.size() << std::endl; //we print the size
+	std::cout << "size after popping the element from the top: " << mstack.size() << std::endl; //we print the size
 
 	mstack.push(3); //we add 3 in the stack
 	mstack.push(5); //we add 5 in the stack
@@ -31,14 +31,14 @@ int main()
 	//[...]
 	mstack.push(0); //we add 0 in the stack
 
-	MutantStack<int>::iterator it = mstack.begin(); //we create an iterator to the begin of the stack
-	MutantStack<int>::iterator ite = mstack.end(); //we create an iterator to the end of the stack
+	 MutantStack<int>::iterator it = mstack.begin(); //we create an iterator to the begin of the stack
+	 MutantStack<int>::iterator ite = mstack.end(); //we create an iterator to the end of the stack
 
-	++it;//not sure why to do this...
+	++it;//to make sure the iterator works properly
 	--it;
 	while (it != ite) //while the iterators are not the same
 	{
-		std::cout << *it << std::endl; //we print the value at the iterator
+		std::cout << "value: " << *it << std::endl; //we print the value at the iterator
 		++it; //we move the iterator (the one from begin())
 	}
 	std::stack<int> s(mstack); //to test the constructor by copy
