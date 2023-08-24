046213b1c301dbbb04933ea6fad279d080874c85
Valerie <zoou2foo@gmail.com>
2023-08-24 13:49:43 -0400

    ex02cpp09 works for vector

--- module_09/ex02/src/PmergeMe.hpp
+++ module_09/ex02/src/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 15:46:59 by vjean             #+#    #+#             */
-/*   Updated: 2023/08/24 13:40:28 by vjean            ###   ########.fr       */
+/*   Updated: 2023/08/24 13:49:01 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,72 +69,32 @@ template<typename T>
 void	sortNmerge(std::vector<T>& leftVec, std::vector<T>& rightVec, std::vector<T>& vec)
 {
 	std::vector<T> tmp;
-	typename std::vector<T>::iterator it = vec.begin();
 	typename std::vector<T>::iterator itLeft = leftVec.begin();
 	typename std::vector<T>::iterator itRight = rightVec.begin();
-	// typename std::vector<T>::iterator itTmp = tmp.begin();
-	// typename std::vector<T>::iterator itTmp2 = tmp.begin();
 
-	// std::cout << "itLeft pointe quoi: " << *itLeft << std::endl;
-	// std::cout << "itLeft + 1 pointe quoi: " << *(itLeft + 1) << std::endl;
-	// std::cout << "itRight pointe quoi: " << *itRight << std::endl;
-	// std::cout << "itRight + 1 pointe quoi: " << *(itRight + 1) << std::endl;
 	while (itLeft != leftVec.end() && itRight != rightVec.end())
 	{
-		if (*itLeft > *(++itLeft))//maybe segfault
-		{
-			// *itTmp = *itLeft;
-			// *itLeft = *(itLeft + 1);
-			// *(itLeft + 1) = *itTmp;
-			tmp.push_back(*(--itLeft));
-			tmp.push_back(*(++itLeft));
-			leftVec.pop_back();
-			leftVec.pop_back();
-			leftVec.push_back(tmp.back());
-			leftVec.push_back(tmp.front());
-			tmp.pop_back();
-			tmp.pop_back();
-		}
-		if (*itRight > *(++itRight))
-		{
-			// *itTmp = *itRight;
-			// *itRight = *(itRight + 1);
-			// *(itRight + 1) = *itTmp;
-			tmp.push_back(*(--itRight));
-			tmp.push_back(*(++itRight));
-			rightVec.pop_back();
-			rightVec.pop_back();
-			rightVec.push_back(tmp.back());
-			rightVec.push_back(tmp.front());
-			tmp.pop_back();
-			tmp.pop_back();
-		}
 		if (*itLeft < *itRight)
 		{
-			// *it = *itLeft;
-			// itLeft++;
-			vec.insert(it, *itLeft); //
-			leftVec.erase(itLeft);
-			//itLeft++;
+			tmp.push_back(*itLeft);
+			++itLeft;
 		}
 		else
 		{
-			*it = *itRight;
-			itRight++;
+			tmp.push_back(*itRight);
+			++itRight;
 		}
-		it++;
 	}
-	while (itLeft != leftVec.end())
+	//odd numbers
+	tmp.insert(tmp.end(), itLeft, leftVec.end());
+	tmp.insert(tmp.end(), itRight, rightVec.end());
+
+	//time to insert back to the main vec
+	typename std::vector<T>::iterator it = vec.begin();
+	for (typename std::vector<T>::iterator itTmp = tmp.begin(); itTmp != tmp.end(); ++itTmp)
 	{
-		*it = *itLeft;
-		itLeft++;
-		it++;
-	}
-	while (itRight != rightVec.end())
-	{
-		*it = *itRight;
-		itRight++;
-		it++;
+		*it = *itTmp;
+		++it;
 	}
 }
 
@@ -169,24 +129,12 @@ void	splitInPairs(std::list<T>& lst) //list
 template<typename T>
 void	splitInPairs(std::vector<T>& vec) //vector
 {
-	std::vector<T> leftVec;
-	std::vector<T> rightVec;
 	size_t length = vec.size();
-	if (length <= 2) //to end recursion
+	if (length <= 1) //to end recursion
 		return;
-	size_t	middle = length / 2;
-	size_t	i = 0;
-	size_t j = 0;
-	for(; i < length; i++)
-	{
-		if (i < middle)
-			leftVec.push_back(vec[i]);
-		else
-		{
-			rightVec.push_back(vec[i]);
-			j++;
-		}
-	}
+	std::vector<T> leftVec(vec.begin(), (vec.begin() + length / 2));
+	std::vector<T> rightVec((vec.begin() + length / 2), vec.end());
+
 	splitInPairs(leftVec);
 	splitInPairs(rightVec);
 	sortNmerge(leftVec, rightVec, vec);
